68426c13456f1daa85bfe72ac8f322497bf22097
Johann Glock <johann.glocks@gmail.com>
2022-05-10 10:06:39 +0200

    Create template for index view

--- apps/crawler/views.py
+++ apps/crawler/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the conference-crawler index.")
+    template = loader.get_template('crawler/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
